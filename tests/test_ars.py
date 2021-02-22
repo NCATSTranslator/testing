e7f9c8ddcce212b055720209651cf1db3567593d
MarkDWilliams <markwilliams2755@gmail.com>
2021-02-22 16:43:48 -0500

    code implemented to check for lack of results for queries

--- tests/test_ars.py
+++ tests/test_ars.py
@@ -5,8 +5,12 @@ from pathlib import Path
 import urllib
 import httpx
 import pytest
+import asyncio
 
-ARS_URL="https://ars.transltr.io/ars/api/"
+#ARS_URL="https://ars.transltr.io/ars/api/"
+ARS_URL="https://ars-dev.transltr.io/ars/api/"
+BASE_PATH=os.path.dirname(os.path.realpath(__file__))
+
 # with open("kps.json", "r") as stream:
 #     KPS = json.load(stream)
 # PAIRS = []
@@ -16,19 +20,28 @@ ARS_URL="https://ars.transltr.io/ars/api/"
 #         payload = json.load(stream)
 #     PAIRS.append((kp_name, url, payload))
 
-def main():
-    pass
-
-if __name__ == '__main__':
-    main()
 
 
-def open_files(relativePath):
-    for filename in os.listdir(os.getcwd()):
-        with open(os.path.join(os.getcwd(),filename),'r') as f:
-            pass
+
+def get_files(relativePath):
+    print("get_files")
+    files=[]
+    my_dir = BASE_PATH+relativePath
+    for filename in os.listdir(my_dir):
+        files.append(os.path.join(my_dir,filename))
+    return files
+
+async def test_not_none():
+    print("test_not_none")
+    files = get_files("/../ars-requests/not-none")
+    for file in files:
+        print("+++ Checking "+file)
+        with open(file, "r") as f:
+            query = json.load(f)
+            await has_results(query)
 
 async def call_ars(payload, url=ARS_URL+"submit"):
+    print("call_ars")
     async with httpx.AsyncClient(verify=False) as client:
         response = await client.post(
             url,
@@ -43,40 +56,59 @@ async def must_contain_curie(curie,query):
     results = get_children(query)
 
 async def has_results(query):
-    children = get_children(query)
-    for k, v in children:
-        results = v['fields']['data']['message']['results']
-        if(len(results)>0):
-            pass
-        else:
-            print(k+" has no results for \n")
-            print(print(json.dumps(json.loads(query), indent=4, sort_keys=True)))
+    print("has_results")
+    children = await get_children(query)
+    for child in children:
+        agent = child[0]
+        childData=child[1]
+        try:
+            results = childData['fields']['data']['message']['results']
+            if(not len(results)>0):
+                print(agent +" has no results for \n")
+                print(json.dumps(query, indent=4, sort_keys=True))
+            else:
+                print(agent +" has "+str(len(results))+" results")
+        except (TypeError, KeyError) as e:
+            print(agent +" has no results for \n")
+            print(json.dumps(query, indent=4, sort_keys=True))
+
 
 async def get_children(query,url=None):
+    print("get_children")
     children = []
-    response = call_ars(query)
+    response = await call_ars(query)
     pk = response['pk']
-    with urllib.request.urlopen(ARS_URL+"messages/"+pk+"?trace=y") as url:
-        data = json.loads(url.read().decode())
+    if (url is None):
+        url = ARS_URL+"messages/"+pk+"?trace=y"
+    async with httpx.AsyncClient(verify=False) as client:
+        r = await client.get(
+            url,
+            timeout=10.0
+        )
+    data = r.json()
     for child in data['children']:
         agent = child['actor']['agent']
         childPk=child['message']
-        with urllib.request.urlopen(ARS_URL+"messages/"+childPk) as url:
-            childData = json.loads(url.read().decode())
+        childUrl=ARS_URL+"messages/"+childPk
+        # with urllib.request.urlopen(ARS_URL+"messages/"+childPk) as url:
+        #     childData = json.loads(url.read().decode())
+        #     children.append(
+        #         {agent,childData}
+        #     )
+        async with httpx.AsyncClient(verify=False) as client:
+            childResponse = await client.get(
+                childUrl,
+                timeout=10.0
+            )
+            childData = childResponse.json()
             children.append(
-                {agent,childData}
+                [agent,childData]
             )
     return children
 
-
-@pytest.mark.parametrize("kp_name,url,payload", PAIRS)
 @pytest.mark.asyncio
-async def test_one_hop(kp_name, url, payload):
-    """Test one-hop queries."""
-    response_json = await call_kp(url, payload)
+async def main():
+    result = await test_not_none()
 
-    responses_path = Path("responses")
-    responses_path.mkdir(parents=True, exist_ok=True)
-    filename = responses_path / f"{kp_name}.json"
-    with open(filename, "w") as stream:
-        json.dump(response_json, stream, indent=4)
+if __name__ == '__main__':
+    asyncio.run(main())

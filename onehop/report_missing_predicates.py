ab81fef022d0064678fd335d7f658a2dd914179e
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2021-03-15 16:01:14 -0700

    add a script for generating a TSV of predicates missing from biolink-model

---
+++ onehop/report_missing_predicates.py
@@ -0,0 +1,67 @@
+import csv
+import bmt
+import requests
+import create_templates
+from biothings_explorer.smartapi_kg.dataload import load_specs
+
+tk = bmt.Toolkit('https://raw.githubusercontent.com/biolink/biolink-model/1.6.0/biolink-model.yaml')
+tsv_file = open("missing_predicates.tsv", "w")
+tsv_writer = csv.writer(tsv_file, delimiter='\t')
+
+
+def aggregate_missing_predicates():
+    specs = load_specs()
+    for spec in specs:
+        if not 'x-translator' in spec['info']:
+            continue
+        url = spec['servers'][0]['url']
+        print(url)
+        apititle = '_'.join(spec['info']['title'].split())
+        if url.endswith('/'):
+            url = url[:-1]
+        predicates_url = f'{url}/predicates'
+        is_trapi, predicates = create_templates.get_predicates(predicates_url)
+        if is_trapi:
+            dump_trapi_predicate_results(apititle, predicates_url, predicates)
+        else:
+            dump_smartapi_predicate_results(spec['info']['title'])
+
+
+def dump_smartapi_predicate_results(apititle):
+    """Create a template for a single REST-style KP smartAPI entry"""
+    metakgurl = f'https://smart-api.info/api/metakg?api={apititle}'
+    response = requests.get(metakgurl)
+    for kgrecord in response.json()['associations']:
+        subject = kgrecord.get('subject')
+        object = kgrecord.get('object')
+        predicate = kgrecord.get('predicate')
+        api = kgrecord.get('api')
+        x_translator = api.get('x-translator')
+        if in_biolink_model(predicate):
+            print(predicate + " is in biolink model")
+        else:
+            if x_translator is not None:
+                for team in x_translator.get('team'):
+                    tsv_writer.writerow([subject, predicate, object, team])
+
+
+def in_biolink_model(predicate):
+    is_predicate = tk.is_predicate(predicate)
+    return is_predicate
+
+
+def dump_trapi_predicate_results(url, predicates):
+        for source in predicates:
+            for target in predicates[source]:
+                for ptype in predicates[source][target]:
+                    predicate = ptype
+                    object = target
+                    subject = source
+                    if in_biolink_model(predicate):
+                        print(predicate + " is in biolink model")
+                    else:
+                        tsv_writer.writerow([subject, predicate, object, url])
+
+
+if __name__ == '__main__':
+    aggregate_missing_predicates()
